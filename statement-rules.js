a49b06bba692c92b2ec46083b2926d54971b655b
mmsaid <55032499+mmsaid@users.noreply.github.com>
2019-12-20 15:32:32 -0500

    Update statement-rules.js

--- statement-rules.js
+++ statement-rules.js
@@ -8,6 +8,6 @@ function submit()
 
     if (a > 35 && c.toUpperCase() == "DECISIONS" || v.toUpperCase() == "VIP") 
     { 
-      result.innerHTML = ('True'); 
-    } else { result.innerHTML = ('False'); }
+      result.innerHTML = ('TRUE'); 
+    } else { result.innerHTML = ('FALSE'); }
 }
