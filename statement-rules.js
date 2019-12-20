b04860b53372c7a6897e4c60c44651d45fdd9950
mmsaid <55032499+mmsaid@users.noreply.github.com>
2019-12-20 15:22:22 -0500

    Add files via upload

---
+++ statement-rules.js
@@ -0,0 +1,14 @@
+
+function submit()
+{
+    a = document.getElementById("a").value; // age
+    c = document.getElementById("c").value; // company
+    v = document.getElementById("v").value; // vip
+    result = document.getElementById("result"); //result..
+
+    if (a > 35 && c.toUpperCase() == "DECISIONS" || v.toUpperCase() == "VIP") 
+    { 
+      result.innerHTML = ('true'); 
+    } else { result.innerHTML = ('false'); }
+
+}
